a10407157a79bde4fe337c79b0ac2a56a826eb85
xRyuzu <lfrodriguezr@outlook.com>
2023-05-29 00:16:35 -0500

    soporte darkmode

--- main.js
+++ main.js
@@ -61,3 +61,44 @@ copyBtn.addEventListener("click", () => {
     },
   });
 });
+
+const sun = document.querySelector("#sun");
+const moon = document.querySelector("#moon");
+
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const iconToggle = () => {
+  moon.classList.toggle("hidden");
+  sun.classList.toggle("hidden");
+};
+
+const themeCheck = () => {
+  if (userTheme === "dark" || (userTheme === null && systemTheme)) {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    sun.classList.remove("hidden");
+    return;
+  }
+
+  localStorage.setItem("theme", "light");
+  moon.classList.remove("hidden");
+};
+
+const themeSwitch = () => {
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+    iconToggle();
+    return;
+  }
+
+  document.documentElement.classList.add("dark");
+  localStorage.setItem("theme", "dark");
+  iconToggle();
+};
+
+sun.addEventListener("click", themeSwitch);
+moon.addEventListener("click", themeSwitch);
+
+themeCheck();
